de2eea519c15cf92b5ca775b7b4d7ac96de99238
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-02-05 15:52:09 -0500

    gg

--- modulos/modulo2/Typeannotations.js
+++ modulos/modulo2/Typeannotations.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var nome = 'oia';
 console.log(nome);
 var animais = ['Leao', 'Gato', 'Panda'];
@@ -19,3 +28,39 @@ if (concluido) {
 else {
     console.log('tarefa nao concluida');
 }
+// array 
+var frutas = ['maca', 'laranja'];
+var frutas1 = ['maca', 'laranja'];
+frutas.push('mamaco');
+console.log(frutas);
+var listaDeNumeros = [1, 2, 3, 4];
+listaDeNumeros = __spreadArray(__spreadArray([], listaDeNumeros, true), [5, 6, 7, 8, 9, 10], false);
+console.log(listaDeNumeros);
+var linguagens = ['jsava', 'pythin', 'php'];
+function Funcaolinga(linguagens) {
+    linguagens.forEach(function (element) {
+        console.log(element);
+    });
+}
+Funcaolinga(linguagens);
+//tuple
+var pessoa;
+pessoa = ['Caue', 'bankai', 12];
+console.log(pessoa);
+var frufru = ['mama', 'ajaz'];
+var listadefrutsa = __spreadArray([5, true], frufru, true);
+function listarPessoas(nomes, idades) {
+    return [nomes, idades];
+}
+var pessaaa = listarPessoas(['Caue', 'joao'], [11, 22]);
+console.log(pessaaa);
+function CriarPessoa() {
+    var nome = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nome[_i] = arguments[_i];
+    }
+    return __spreadArray([], nome, true);
+}
+;
+console.log(CriarPessoa('Caue', 'Falcao'));
+console.log(CriarPessoa('Caue', 'jose', 'Falcao'));
--- modulos/modulo2/Typeannotations.ts
+++ modulos/modulo2/Typeannotations.ts
@@ -32,3 +32,59 @@ if (concluido){
 else{
     console.log('tarefa nao concluida')
 }
+
+// array 
+
+let frutas : string[] = ['maca','laranja'];
+
+let frutas1 : Array<string>= ['maca','laranja'];
+
+frutas.push('mamaco')
+console.log(frutas)
+
+let listaDeNumeros : Array<number> = [1,2,3,4];
+
+listaDeNumeros = [...listaDeNumeros , 5, 6, 7, 8, 9 , 10];
+
+console.log(listaDeNumeros)
+
+let linguagens : Array<string> = ['jsava','pythin','php'];
+
+function Funcaolinga(linguagens:string[]){
+    linguagens.forEach(element => {
+        console.log(element)
+    });
+}
+
+Funcaolinga(linguagens)
+
+//tuple
+
+let pessoa: [string , string , number];
+
+pessoa = ['Caue' , 'bankai' , 12];
+
+console.log(pessoa)
+
+let frufru: [string, ...string[]] = ['mama','ajaz']
+
+let listadefrutsa : [number,boolean , ...Array<string>] = [5,true,...frufru]
+
+
+function listarPessoas(nomes:Array<string>,idades:Array<number>){
+    return [nomes,idades]
+}
+
+let pessaaa = listarPessoas(['Caue','joao'],[11,22])
+console.log(pessaaa) 
+
+type Nome = 
+    | [priemiroNome:string,sobrenome : string] | [priemiroNome:string,nomedomeio:string,sobrenome : string];
+
+
+    function CriarPessoa(...nome:Nome){
+        return [...nome]
+    };
+
+console.log(CriarPessoa('Caue','Falcao'))
+console.log(CriarPessoa('Caue','jose','Falcao'))
